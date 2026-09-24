a4ff644ecf7fed5b2123202c1cbdc69ca0a4535a
cdostay@gmail.com <cdostay@gmail.com>
2026-09-24 20:26:52 +0000

    product ve ship schemaları ayarlandı

---
+++ models/staging/course16/stg_course16__gwz_product.sql
@@ -0,0 +1,21 @@
+with 
+
+source as (
+
+    select * from {{ source('course16', 'gwz_product') }}
+
+),
+
+renamed as (
+
+    select
+        products_id,
+        CAST(purchase_price AS FLOAT64) AS purchase_price
+    from source
+
+)
+
+select * from renamed
+)
+
+select * from renamed
