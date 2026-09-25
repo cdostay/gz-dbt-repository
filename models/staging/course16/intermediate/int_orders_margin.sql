f3bc9fed90cbb097ee8687d00bc6b5a769911d87
cdostay@gmail.com <cdostay@gmail.com>
2026-09-25 12:44:53 +0000

    order margin addded

---
+++ models/staging/course16/intermediate/int_orders_margin.sql
@@ -0,0 +1,16 @@
+WITH sales_margin AS (
+
+    SELECT *
+    FROM {{ ref('sales_margin') }}
+
+)
+
+SELECT
+    orders_id,
+    MIN(date_date) AS date_date,
+    SUM(turnover) AS gelir,
+    SUM(qty) AS miktar,
+    SUM(`satın_alma_maliyeti`) AS `satın_alma_maliyeti`,
+    SUM(marj) AS marj
+FROM sales_margin
+GROUP BY orders_id
