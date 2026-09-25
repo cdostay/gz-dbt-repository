c15a6798c49df33766edbaaeea6c06873e41a8fd
cdostay@gmail.com <cdostay@gmail.com>
2026-09-25 20:46:30 +0000

    reklam kampanyaları eklendi

---
+++ models/staging/course16/staging/stg_course16__gz_adwords.sql
@@ -0,0 +1,23 @@
+WITH source AS (
+
+    SELECT *
+    FROM {{ source('course16', 'gz_adwords') }}
+
+),
+
+renamed AS (
+
+    SELECT
+        date_date,
+        paid_source,
+        campaign_key,
+        campaign_name,
+        cost,
+        impression,
+        click
+    FROM source
+
+)
+
+SELECT *
+FROM renamed
